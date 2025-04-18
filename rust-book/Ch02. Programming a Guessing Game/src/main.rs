fd483ebe0b092d1df4880421538b94ccbb3d2b87
richarddalves <54530477+richarddalves@users.noreply.github.com>
2025-04-18 19:46:34 -0300

    Quitting after a correct guess and handling invalid input

--- rust-book/Ch02. Programming a Guessing Game/src/main.rs
+++ rust-book/Ch02. Programming a Guessing Game/src/main.rs
@@ -6,10 +6,9 @@ fn main() {
     println!("Gues the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
 
     loop {
-        print!("Please, input your guess: ");
+        println!("Please, input your guess: ");
 
         let mut guess = String::new();
 
@@ -17,7 +16,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {guess}");
 
