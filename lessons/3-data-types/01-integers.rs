fa4100a3e1d2c0a7399376b0296a9daefa1c6bd5
Richard Alves <54530477+richarddalves@users.noreply.github.com>
2025-05-04 18:11:36 -0300

    added files about integers and usize/isize types

---
+++ lessons/3-data-types/01-integers.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let sixteen_bit_signed: i16 = -32500;
+    let sixteen_bit_unsigned: u16 = 64000;
+
+    let thirty_two_bit_signed = -2_147_483_648;
+    let thirty_two_bit_unsigned: u32 = 4_294_967_295;
+
+    let some_value = 20u16;
+}
---
+++ lessons/3-data-types/02-usize-and-isize.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let days: usize = 55; // Em um computador de 32 bits, isso será um u32. Em um computador de 64 bits, será um u64.
+    let years: isize = -15_000; // A mesma coisa se aplica, mas com i32 e i64.
+}
