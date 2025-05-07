8c684e50ae87bd49309a48acf4abeec124b6d28a
Richard Alves <54530477+richarddalves@users.noreply.github.com>
2025-05-07 20:42:08 -0300

    Math Operations

---
+++ lessons/3-data-types/07-math-operations.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let addition = 5 + 4;
+    let subtraction = 10 - 6;
+    let multiplication = 3 * 4;
+    println!("Addition: {addition}, subtraction: {subtraction}, multiplication: {multiplication}");
+ 
+    let floor_division = 12 / 7;
+    println!("{floor_division}");
+    
+    let decimal_division = 12.0 / 7.0;
+    println!("{decimal_division}");
+ 
+    let remainder = 12 % 7;
+    println!("{remainder}");
+ }
