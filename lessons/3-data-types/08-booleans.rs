933ae2182b0b6e35e150ea9a8fc354311714759e
Richard Alves <54530477+richarddalves@users.noreply.github.com>
2025-05-08 14:01:27 -0300

    "and" logic with "&&"

--- lessons/3-data-types/08-booleans.rs
+++ lessons/3-data-types/08-booleans.rs
@@ -23,11 +23,17 @@ fn main() {
  
     println!("{}", 10 == 10); // true
     println!("{}", 10 != 10); // false
- 
     
     println!("{}", 17.22 == 17.2200); // true
     println!("{}", 17.22 == 17.22001); // false
     println!("{}", 17.22 == 17.2200000000000001); // true
  
-    println!("{}", 0.1 + 0.2 == 0.3); // false
+    println!("{}\n", 0.1 + 0.2 == 0.3); // false
+ 
+    // And Logic with &&
+    let purchased_ticket = true;
+    let plane_on_time = true;
+    let making_event = purchased_ticket && plane_on_time;
+ 
+    println!("It is {} that I will arrive as expected.", making_event);
  }
