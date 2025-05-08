fc5d4a4ebaa8a94a03909fc543bb67a8df71ccdc
Richard Alves <54530477+richarddalves@users.noreply.github.com>
2025-05-08 13:48:42 -0300

    equality and inequality operators

--- lessons/3-data-types/08-booleans.rs
+++ lessons/3-data-types/08-booleans.rs
@@ -7,11 +7,27 @@ fn main() {
     let age: i32 = 21;
     let is_adult = age >= 18;
     println!("{is_adult}");
-    println!("{} {}", age.is_positive(), age.is_negative());
+    println!("{} {}\n", age.is_positive(), age.is_negative());
  
     // Boolean Inversion with !
     let can_see_rated_r_movie = age >= 17;
     let cannot_see_rated_r_movie = !can_see_rated_r_movie;
+    println!("I am {age} years old and. Can I not see this scary movie? {cannot_see_rated_r_movie}.\n");
  
-    println!("I am {age} years old and. Can I not see this scary movie? {cannot_see_rated_r_movie}.");
+    // Equality and Inequality Operators
+    println!("{}", "Coke" == "Pepsi"); // false
+    println!("{}", "Coke" != "Pepsi"); // true
+    println!("{}", "Coke" == "coke"); // false
+    println!("{}", "Coke" == "Coke "); // false
+    println!("{}", "Coke" == "Coke");  // true
+ 
+    println!("{}", 10 == 10); // true
+    println!("{}", 10 != 10); // false
+ 
+    
+    println!("{}", 17.22 == 17.2200); // true
+    println!("{}", 17.22 == 17.22001); // false
+    println!("{}", 17.22 == 17.2200000000000001); // true
+ 
+    println!("{}", 0.1 + 0.2 == 0.3); // false
  }
