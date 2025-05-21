bdd42376ade6ef9f05a986d1d02e6260cc59f888
Richard Alves <54530477+richarddalves@users.noreply.github.com>
2025-05-21 15:36:33 -0300

    lesson(data types): reading and writing array elements

--- lessons/3-data-types/10-arrays.rs
+++ lessons/3-data-types/10-arrays.rs
@@ -1,7 +1,18 @@
 fn main() {
+   // The Array Type
    let numbers: [i32; 6] = [4, 8, 15, 16, 23, 26];
    let apples = ["Granny Smith", "McIntosh", "Red Delicious"];
    println!("Length: {}", apples.len());
 
    let currency_rates: [f64; 0] = [];
+
+   // Reading and Writing Array Elements
+   let mut seasons = ["Spring", "Summer", "Fall", "Winter"];
+
+   let _first = seasons[0];
+   let _last = seasons[seasons.len() - 1];
+
+   println!("{}", seasons[2]);
+   seasons[2] = "Autumn";
+   println!("{}", seasons[2]);
 }
