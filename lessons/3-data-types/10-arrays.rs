a984ee05da31be8a7af8df75f83e40ee0dd60830
Richard Alves <54530477+richarddalves@users.noreply.github.com>
2025-05-21 12:42:23 -0300

    the array type

---
+++ lessons/3-data-types/10-arrays.rs
@@ -0,0 +1,7 @@
+fn main() {
+   let numbers: [i32; 6] = [4, 8, 15, 16, 23, 26];
+   let apples = ["Granny Smith", "McIntosh", "Red Delicious"];
+   println!("Length: {}", apples.len());
+
+   let currency_rates: [f64; 0] = [];
+}
